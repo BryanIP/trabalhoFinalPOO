508ee7616bcc9dcbd9876375f276b42b1ba4c5ee
Bryan Bento <bryanrodriguesip@gmail.com>
2019-11-09 16:08:17 -0300

    Criação das classes

---
+++ AgenciaViagens/src/Agencia.java
@@ -0,0 +1,46 @@
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Bryan
+ */
+public class Agencia extends Empresa {
+    private int codigo;
+    private ArrayList<Pacote> pacotes;
+
+    public Agencia(int codigo, String cnpj, String razaoSocial, String nomeFantasia, String inscricaoEstadual, String inscricaoMunicipal, Endereco endereco, Date dataAbertura, boolean matriz) {
+        super(cnpj, razaoSocial, nomeFantasia, inscricaoEstadual, inscricaoMunicipal, endereco, dataAbertura, matriz);
+        this.codigo = codigo;
+    }
+    
+    public void setPacotes(Pacote pacote) {
+        this.pacotes.add(pacote);
+    }
+
+    public void setPacotes(ArrayList<Pacote> pacotes) {
+        this.pacotes = pacotes;
+    }
+
+/*    
+    public Pacote getPacotes(String codigo) {
+        for (int i=0; i<pacotes.size(); i++) {
+            if (codigo == pacotes.get(i).getCodigo()) {
+                return pacotes.get(i);
+            }
+        }
+    }
+*/    
+    
+    public ArrayList<Pacote> getPacotes() {
+        return pacotes;
+    }
+    
+}
---
+++ AgenciaViagens/src/Atracao.java
@@ -0,0 +1,55 @@
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Bryan
+ */
+public class Atracao {
+    private int codigo;
+    private String tipo;
+    private String nome;
+    private double preco;
+    private Date data;
+
+    public Atracao(int codigo, String tipo, String nome, double preco, Date data) {
+        this.codigo = codigo;
+        this.tipo = tipo;
+        this.nome = nome;
+        this.preco = preco;
+        this.data = data;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    @Override
+    public String toString() {
+        return tipo + ' ' + nome + ", " + data + ", " + preco + " - #" + codigo;
+    }
+    
+    
+}
