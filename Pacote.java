5ba45157d63d192bee68877e8e5768bcee0f894f
PeFaustino <47853988+PeFaustino@users.noreply.github.com>
2019-11-20 21:25:39 -0300

    Novo constructor Agencia e novo parametro Pacote

---
+++ Pacote.java
@@ -0,0 +1,55 @@
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Bryan
+ */
+public class Pacote {
+    private String codigo;
+    private ArrayList<Cidade> cidades;
+    private double preco;
+    private Date dataSaida;
+    private Date dataChegada;
+    private int qtdDias;
+    private String outrosDetalhes;
+
+    public Pacote(String codigo, ArrayList<Cidade> cidades, double preco, Date dataSaida, Date dataChegada, int qtdDias, String outrosDetalhes) {
+        this.codigo = codigo;
+        this.cidades = cidades;
+        this.preco = preco;
+        this.dataSaida = dataSaida;
+        this.dataChegada = dataChegada;
+        this.qtdDias = qtdDias;
+        this.outrosDetalhes = outrosDetalhes;
+    }
+
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setOutros(String outrosDetalhes) {
+        this.outrosDetalhes = outrosDetalhes;
+    }
+    
+    @Override
+    public String toString() {
+        String txt = "Pacote #" + codigo + " Saída " + dataSaida + " Chegada " + dataChegada + "( " + qtdDias + " dias)";
+        txt = txt + "\nCidades: \n";
+        
+        for (int i=0; i<cidades.size(); i++) {
+            txt = txt + cidades.get(i) + "\n";
+        }
+        
+        return txt;
+    }
+    
+}
